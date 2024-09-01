f281e1e02316f294214ba0d5478bdf7135728247
Ritik <ritik85@gmail.com>
2024-09-02 03:24:06 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,36 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/app.scss";
 import { createContext } from "react";
 
-export const server="https://frontend-to-do-app.onrender.com/api/v1";
-export const Context= createContext({ isAuthenticated:false });
+export const server = "https://backend-to-do-app-1.onrender.com/api/v1";
 
-const AppWrapper=()=>{
-  const [isAuthenticated,setIsAuthenticated]=useState(false);
-  const [loading,setLoading]=useState(false);
-  const [user,setUser]=useState({});
+export const Context = createContext({ isAuthenticated: false });
+
+const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState({});
 
   return (
-    <Context.Provider value={{
-      isAuthenticated,
-      setIsAuthenticated,
-      loading,
-      setLoading,
-      user,
-      setUser,
-    }}>
+    <Context.Provider
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        loading,
+        setLoading,
+        user,
+        setUser,
+      }}
+    >
       <App />
     </Context.Provider>
   );
-  
 };
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppWrapper />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
